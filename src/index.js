502f2fb0c43f778a97baf07c8fbecbc968fd87ae
BeeFarsh <farsh@localhost.localdomain>
2022-09-16 16:11:43 +0100

    blog app with routing

--- src/index.js
+++ src/index.js
@@ -3,6 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
+
+axios.defaults.baseURL = "http://jsonplaceholder.typicode.com";
+axios.defaults.headers.common["Authorization"] = "AUTH TOKEN";
+axios.defaults.headers.post["Content-type"] = "application/json";
+
+axios.interceptors.request.use(request => {
+  console.log(request)
+  return request;
+}, error => {
+  console.log(error)
+  return Promise.reject(error)
+})
+
+axios.interceptors.response.use(response => {
+  console.log(response);
+  return response;
+}, error => {
+  console.log(error);
+  return Promise.reject(error)
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
