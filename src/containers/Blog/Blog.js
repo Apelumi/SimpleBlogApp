34d35751ac5e77e2f6ff106fca87992b3ef75115
BeeFarsh <farsh@localhost.localdomain>
2022-09-16 20:11:16 +0100

    changes made to the routing by changing our anchor tag to use the react-dom link property

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -6,7 +6,7 @@ import React, { Component } from 'react';
 // import NewPost from './NewPost/NewPost';
 import './Blog.css';
 import Posts from './Posts/Posts';
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, Link } from "react-router-dom"
 import NewPost from './NewPost/NewPost';
 
 
@@ -56,8 +56,12 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to={{
+                                pathname: "/new-post",
+                                hash: "#submit", //these two atrributes won't work coz they are not doing anything on here
+                                search: "?quick-search-submit"
+                            }}>New Post</Link></li>
                         </ul>
                     </nav>
                 </header>
